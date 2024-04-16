45485ab0d64de7a3c7df43ed586eab9a5f27b105
Benjamin-sa <Benkee.sauter@gmail.com>
2024-04-16 21:28:20 +0200

    Add video streaming functionality to Flask server

--- Galactic_cheese_master/server.py
+++ Galactic_cheese_master/server.py
@@ -1,30 +1,54 @@
-from flask import Flask, render_template
+from flask import Flask, render_template, Response
+import cv2
 # import RPi.GPIO as GPIO
 
 app = Flask(__name__, template_folder='Client_Side')
 
+#oude gsm voor camera
+camera = cv2.VideoCapture('http://192.168.0.142:8080/video')
 
 @app.route('/')
 def home():
     return render_template('index.html')
 
+def generate_frames():
+    while True:
+        success, frame = camera.read()
+        if not success:
+            break
+        else:
+            ret, buffer = cv2.imencode('.jpg', frame)
+            frame = buffer.tobytes()
+            yield (b'--frame\r\n'
+                   b'Content-Type: image/jpeg\r\n\r\n' + frame + b'\r\n')
+
+@app.route('/video_feed')
+def video_feed():
+    return Response(generate_frames(), mimetype='multipart/x-mixed-replace; boundary=frame')
 
 @app.route('/command/<cmd>', methods=['POST'])
-def handle_command(cmd):
+@app.route('/command/<cmd>/<value>', methods=['POST'])
+def handle_command(cmd, value=None):
     if cmd == 'draai_links':
-        # code om de robot links te draaien
+        print('Links draaien')
         return 'linksdraaien'
     elif cmd == 'draai_rechts':
-        # Code om de robot rechts te draaien
+        print('Rechts draaien')
         return 'rechtsdraaien'
     elif cmd == 'schiet':
-        # Code om te schieten
+        print('Schieten')
         return 'geschoten'
     elif cmd == 'herlaad':
-        # Code om te herladen
+        print('Herladen')
         return 'herladen'
+    elif cmd == 'stop':
+        print('Stop')
+        return 'gestopt'
+    elif cmd == 'updateMotorPower':
+        print(f'Update motor power naar {value}')
+        
+        return 'Motor power updated'
     else:
         return 'Geen commando', 400
-
 if __name__ == '__main__':
-    app.run()
+    app.run(host='0.0.0.0')
