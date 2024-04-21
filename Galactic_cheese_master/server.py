fd79ced01f8548168a961746ee823af6a23b1d0f
Benjamin-sa <Benkee.sauter@gmail.com>
2024-04-21 20:55:38 +0200

    Refactor CSS styles in index.html and instellingen.html

--- Galactic_cheese_master/server.py
+++ Galactic_cheese_master/server.py
@@ -1,14 +1,14 @@
 from flask import Flask, render_template, Response, url_for, jsonify
 import cv2
 import time
-import math
 import os
 import numpy as np
+from datetime import datetime
 
 app = Flask(__name__, template_folder='Client_Side')
 
 # Oude GSM voor camera
-camera = cv2.VideoCapture('video\IMG_0562.MOV')
+camera = cv2.VideoCapture('http://192.168.137.142:8080/video')
 
 @app.route('/')
 def home():
@@ -50,62 +50,27 @@ def video_feed():
 
 @app.route('/analyze')
 def analyze_with_angles(image_path=None):
-    if image_path is None:
-        success, frame = camera.read()
-    else:
-        frame = cv2.imread(image_path)
-        success = frame is not None
+    for _ in range(10):  
+        camera.grab()
+    success, frame = camera.read()
     if success:
         if frame.shape[0] > frame.shape[1]:
             frame = cv2.rotate(frame, cv2.ROTATE_90_CLOCKWISE)
-            
-        gray = cv2.cvtColor(frame, cv2.COLOR_BGR2GRAY)
-        ret, thresh = cv2.threshold(gray, 127, 255, cv2.THRESH_BINARY_INV)
-
-        contours, _ = cv2.findContours(thresh, cv2.RETR_TREE, cv2.CHAIN_APPROX_SIMPLE)
-
-        
-        centroids = []
-
-        for contour in contours:
-            area = cv2.contourArea(contour)
-            if area > 100:  
-                M = cv2.moments(contour)
-                if M["m00"] != 0:
-                    cX = int(M["m10"] / M["m00"])
-                    cY = int(M["m01"] / M["m00"])
-                    centroids.append((cX, cY))
-
-                    
-                    cv2.drawContours(frame, [contour], -1, (0, 255, 0), 2)
-                    cv2.circle(frame, (cX, cY), 7, (255, 255, 255), -1)
-
-        frame_center_x = frame.shape[1] // 2
-        field_of_view = 60  
-        angles_to_centroids = []
-
-        for (cX, cY) in centroids:
-            angle = (cX - frame_center_x) / frame_center_x * (field_of_view / 2)
-            angles_to_centroids.append(angle)
-            
-            cv2.putText(frame, f"{angle:.2f}", (cX - 10, cY - 10),
-                        cv2.FONT_HERSHEY_SIMPLEX, 0.5, (255, 255, 255), 2)
 
         static_path = os.path.join(app.root_path, 'static')
         if not os.path.exists(static_path):
             os.makedirs(static_path)
 
-        save_path = os.path.join(static_path, 'processed_image.jpg')
+        timestamp = datetime.now().strftime("%Y%m%d%H%M%S")
+        filename = f'processed_image_{timestamp}.jpg'
+        save_path = os.path.join(static_path, filename)
         cv2.imwrite(save_path, frame)
 
-        image_url = url_for('static', filename='processed_image.jpg')
+        image_url = url_for('static', filename=filename)
         return jsonify({'image_url': image_url})
 
     else:
         return jsonify({'error': 'Unable to capture image'}), 500
-
-
-analyze_with_angles('video/trou-madame02.png')
 
 
 
