300f1091245ad9aa5c2d3c8c09bd8a5ee56f1de6
Benjamin-sa <Benkee.sauter@gmail.com>
2024-04-29 18:10:43 +0200

    update

---
+++ Galactic_cheese_master/Controller.py
@@ -0,0 +1,31 @@
+# import RPi.GPIO as GPIO
+import time
+
+motorPin = 6
+stepPin12 = 4
+dirPin1 = 2
+dirPin2 = 3
+button1 = 7
+
+
+# GPIO.setmode(GPIO.BCM)
+
+# for l in range(7): 
+#     GPIO.setup(i,GPIO.OUT)
+
+def shiet():
+    return "pew pew"
+    
+    
+def herladen():
+    return "motor beweegt"
+
+def laser_toggle():
+    return "laser toggle"
+
+
+def draai_links(graden):
+    return "draai links"
+
+def draai_rechts(graden):
+    return "draai rechts"
--- Galactic_cheese_master/server.py
+++ Galactic_cheese_master/server.py
@@ -1,105 +1,59 @@
-from flask import Flask, render_template, Response, url_for, jsonify
-import cv2
-import time
-import os
-import numpy as np
-from datetime import datetime
+from flask import Flask, jsonify, render_template, request
+import requests
+import math
+import Controller
 
 app = Flask(__name__, template_folder='Client_Side')
 
-# Oude GSM voor camera
-camera = cv2.VideoCapture('http://192.168.137.142:8080/video')
+def berekening_draaihoek(x, y, z, w):
+    y = -y  
+    z = -z  
+
+    t3 = +2.0 * (w * z + x * y)
+    t4 = +1.0 - 2.0 * (y * y + z * z)
+    radialen = math.atan2(t3, t4)
+
+    graden = math.degrees(radialen)
+
+    if graden < 0:
+        graden += 360
+
+    return graden
 
 @app.route('/')
-def home():
+def index():
     return render_template('index.html')
 
-@app.route('/instellingen')
-def instellingen():
-    return render_template('instellingen.html')
-
-def generate_frames():
-    while True:
-        success, frame = camera.read()
-        if success:
-            if frame.shape[0] > frame.shape[1]:
-                frame = cv2.rotate(frame, cv2.ROTATE_90_CLOCKWISE)
-
-            ret, buffer = cv2.imencode('.jpg', frame)
-            frame = buffer.tobytes()
-
-            yield (b'--frame\r\n'
-                   b'Content-Type: image/jpeg\r\n\r\n' + frame + b'\r\n')
-        else:
-            print("niet gelukt, Opnieuw verbinden..")
-            opnieuw_verbinden()
-
-def opnieuw_verbinden():
-    global camera
-    camera.release()
-    time.sleep(2) 
-    camera = cv2.VideoCapture('http://192.168.0.142:8080/video')
-    if camera.isOpened():
-        print("opnieuw verbonden!")
-    else:
-        print("weer mislukt. Opnieuw proberen...")
-
-@app.route('/video_feed')
-def video_feed():
-    return Response(generate_frames(), mimetype='multipart/x-mixed-replace; boundary=frame')
-
-@app.route('/analyze')
-def analyze_with_angles(image_path=None):
-    for _ in range(10):  
-        camera.grab()
-    success, frame = camera.read()
-    if success:
-        if frame.shape[0] > frame.shape[1]:
-            frame = cv2.rotate(frame, cv2.ROTATE_90_CLOCKWISE)
-
-        static_path = os.path.join(app.root_path, 'static')
-        if not os.path.exists(static_path):
-            os.makedirs(static_path)
-
-        timestamp = datetime.now().strftime("%Y%m%d%H%M%S")
-        filename = f'processed_image_{timestamp}.jpg'
-        save_path = os.path.join(static_path, filename)
-        cv2.imwrite(save_path, frame)
-
-        image_url = url_for('static', filename=filename)
-        return jsonify({'image_url': image_url})
-
-    else:
-        return jsonify({'error': 'Unable to capture image'}), 500
+@app.route('/rotation')
+def get_rotation():
+    url = 'http://192.168.0.142:8080/sensors.json'
+    try:
+        data = requests.get(url).json()
+        rotation_vector = data['rot_vector']['data'][-1][1]
+        x, y, z, w = rotation_vector[0:4]  # Extract quaternion components
+        hoek_waarde = berekening_draaihoek(x, y, z, w)
+        return jsonify({'hoek': hoek_waarde})
+    except requests.RequestException as e:
+        return jsonify({'fout': 'Failed to fetch data', 'details': str(e)}), 500
+    except KeyError:
+        return jsonify({'error': 'Malformed data received'}), 500
 
 
 
 @app.route('/command/<cmd>', methods=['POST'])
 @app.route('/command/<cmd>/<value>', methods=['POST'])
 def handle_command(cmd, value=None):
-    if cmd == 'draai_links':
-        print('Links draaien')
-        return 'linksdraaien'
+    if cmd == 'shiet':
+        Controller.shiet()
+    elif cmd == 'dc_motor_snelheid':
+        Controller.herladen()
+    elif cmd == 'laser_toggle':
+        Controller.laser_toggle()
+    elif cmd == 'draai_links':
+        Controller.draai_links(value)
     elif cmd == 'draai_rechts':
-        print('Rechts draaien')
-        return 'rechtsdraaien'
-    elif cmd == 'schiet':
-        print('Schieten')
-        return 'geschoten'
-    elif cmd == 'herlaad':
-        print('Herladen')
-        return 'herladen'
-    elif cmd == 'stop':
-        print('Stop')
-        return 'gestopt'
-    elif cmd == 'updateMotorPower':
-        print(f'Update motor power naar {value}')
-        
-        return 'Motor power updated'
-    else:
-        return 'Geen commando', 400
+        Controller.draai_rechts(value)
+    return ({'status': 'success', 'cmd': cmd, 'value': value})
 
-
-    
 if __name__ == '__main__':
-    app.run()
+    app.run(host='0.0.0.0')
